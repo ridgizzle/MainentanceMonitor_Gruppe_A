5c7623554250eae3ebf8e9190fdc1d2f34e73d13
Daniel Stangl <ic21b032@technikum-wien.at>
2022-11-20 16:59:46 +0100

    lol commit2

--- src/main/java/fhtw/bic/maintenancemonitor/controller/MaintenancemonitorController.java
+++ src/main/java/fhtw/bic/maintenancemonitor/controller/MaintenancemonitorController.java
@@ -1,12 +1,12 @@
 package fhtw.bic.maintenancemonitor.controller;
 
 import fhtw.bic.maintenancemonitor.model.Maintenancemonitor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.swing.text.View;
 import java.sql.Date;
@@ -20,24 +20,24 @@ public class MaintenancemonitorController {
 
     @GetMapping("/api/message/set")
     //@ResponseBody
-    public ModelAndView setMessage(@RequestParam(name="m", required=false, defaultValue="Everything works as expected") String message){
+    public String setMessage(@RequestParam(name="m", required=false, defaultValue="Everything works as expected") String message, Model model){
 
+        LocalDateTime dateTime = LocalDateTime.now();
         mm.setStatus(message);
+        mm.setStatusDate(dateTime);
 
-        ModelAndView mv = new ModelAndView();
-        mv.addObject(mm.getStatus(), mm);
-        mv.setViewName("/");
+        model.addAttribute("mm", mm);
+        model.addAttribute("title", "Test-Title");
         //mm.setStatus(message);
         //return mm.setMessage(message);
-        return mv;
+        return "index";
     }
 
-/*
-    @RequestMapping(value = "/api")
-    public String returnView(){
-        return "/";
+    @GetMapping("/")
+    public String index(Model model) {
+
+        return "index";
     }
-*/
 
     @GetMapping("/api/message/reset")
     //@ResponseBody
@@ -49,15 +49,16 @@ public class MaintenancemonitorController {
         mm.setStatusDate(dateTime);
 
         model.addAttribute("mm", mm);
+        model.addAttribute("title", "Test-Title");
         //model.addAttribute("statusDateTime", mm.getStatusDate());
         //return mm.setMessage(message);
-        return "/";
+        return "index";
         //return mm.setStatus("Everything works as expected");
     }
 
-    @GetMapping("/greeting")
+    @GetMapping("/api/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "api/greeting";
     }
 }
