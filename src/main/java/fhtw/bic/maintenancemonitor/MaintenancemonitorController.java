234227582482beefab305a0c3150487bdfacd69c
melle <lebesmm@gmx.at>
2022-11-20 17:10:15 +0100

    simple textfield-submitbutton application

--- src/main/java/fhtw/bic/maintenancemonitor/MaintenancemonitorController.java
+++ src/main/java/fhtw/bic/maintenancemonitor/MaintenancemonitorController.java
@@ -1,29 +1,36 @@
 package fhtw.bic.maintenancemonitor;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MaintenancemonitorController {
+    public static String message = "default message";
 
-    private Maintenancemonitor mm = new Maintenancemonitor();
-
-
-    @GetMapping("/api/message/set")
-    public String setMessage(@RequestParam(name="m", required=false, defaultValue="Everything works as expected") String message){
-        return mm.setMessage(message);
+    public String index(Model model) {
+        formHandler();
+        displayMessage();
+        return "form";
     }
-
-    @GetMapping("/api/message/reset")
-    public String setMessage(){
-        return mm.setMessage("Everything works as expected");
+    @ResponseBody
+    @RequestMapping(value = "message")
+    public String displayMessage(){
+        return message;
     }
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @RequestMapping (method = RequestMethod.GET)
+    @ResponseBody
+    public String formHandler(){
+        String html = "<form method ='post'>" +                     //nach post kann um eine Url erweitert werden action='someURL' --> wie Weblink
+                "<input type='text' name='name' />" +               //name ist der Parametername der in postHandler initiert wird
+                "<input type='submit' value='Submit message' />" +
+                "</form>";                                                //closes form
+        return html;
+    }
+    @RequestMapping (method = RequestMethod.POST)
+    @ResponseBody
+    public String postHandler(HttpServletRequest request){
+        message = request.getParameter("name");
+        return formHandler();
     }
 }
